fa0d1d41ad0034e37cbdb80fe361238ca7ad4942
Giovanne Striquer <97549620+giostriquer@users.noreply.github.com>
2026-09-10 18:39:53 -0300

    fix: package billing resources outside helper code (#48)

--- src-tauri/macos/BrowserBilling/Tests/BrowserBillingTests/NavigationTests.swift
+++ src-tauri/macos/BrowserBilling/Tests/BrowserBillingTests/NavigationTests.swift
@@ -1,7 +1,8 @@
 import Foundation
 import BillingCore
 @main struct NavigationChecks {
-    static func main() {
+    static func main() throws {
+        try checkResources()
         precondition(NavigationPolicy.allows(url: URL(string: "about:blank"), isMainFrame: false))
         precondition(NavigationPolicy.allows(url: URL(string: "https://challenges.cloudflare.com/"), isMainFrame: false))
         precondition(NavigationPolicy.allows(url: URL(string: "https://chatgpt.com/"), isMainFrame: true))
